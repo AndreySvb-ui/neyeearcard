cb5ebaaa43c5a7f433ec28700f847aa308fdf5ff
andrey <ardiente6387@gmail.com>
2024-12-09 21:48:14 +0200

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const button = document.querySelector('.main__button');
+  
+  button.addEventListener('click', () => {
+    alert('Wishing you a joyful and successful New Year!');
+  });
+});
